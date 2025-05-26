f1890c1dbc67f05e818dc25866831f4906dd1991
steve-o <fnjordy@gmail.com>
2025-05-26 18:38:40 +0000

    Export `AppManifest`.

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,6 @@
 // vim: tabstop=8 softtabstop=0 noexpandtab shiftwidth=8 nosmarttab
 export * from './abstract-app.js';
+export * from './app-manifest.js';
 export * from './app-params.js';
 export * from './config-modal.js';
 export * from './web-app.js';
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 // vim: tabstop=8 softtabstop=0 noexpandtab shiftwidth=8 nosmarttab
 
 export * from './abstract-app.js';
+export * from './app-manifest.js';
 export * from './app-params.js';
 export * from './config-modal.js';
 export * from './web-app.js';
